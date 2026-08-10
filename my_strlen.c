1091d657c504e9928a0481db9053a92db965c835
azareelamoki <azareelamoki944@gmail.com>
2026-08-10 19:06:56 +0100

    Udated documentation and comments on strlen function

--- my_strlen.c
+++ my_strlen.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
-size_t my_strlen(const char *s)
+size_t my_strlen(const char *s) //Approche A
 {
     int i = 0;
-    while (s[i] != '\0') {
+    while (s[i] != '\0') {                     // Pacours de la chaine de carcatère à l'aide d'un indice I qui représentera exactement le nombre de caractère lu. 
         i++;
     }
-    return i;
+    return i;                                  // Retour de cet indice car la fonction sers à connaitre le nombre d'élément contenus dans un tableau de caractère
 }
 
-size_t my_strlen_b(const char *s)
+size_t my_strlen_b(const char *s) // Approche B
 {
     size_t n = 0;
-    while (*s != '\0') {
-        n++;
-        s++;
+    while (*s != '\0') {                       // Lecture avec pointeur, plus efficace et directement liée au fonctionnement de la mémoire 
+        n++;                                   // incrémentation d'un indice n au fur et à mesure que le pointeur permet de parcourir les élément du tabeau de caractère en accédant à leur emplacement mémoire
+        s++;                                   // incrémentation du pointeur 
     }
-    return n;
+    return n;                                  // retour de l'indice n représentant exatement le nombre de caractère contenu dans le tabeau
 }
 
-size_t my_strlen_c(const char *s)
+size_t my_strlen_c(const char *s) // Approche C
 {
     // size_t n = 0;
     // while (*s) {
@@ -40,11 +40,11 @@ size_t my_strlen_c(const char *s)
 
     //ou encore
     
-    const char *start = s;
+    const char *start = s;                      //Conservation de l'adresse initiale de la chaine dans start
     
-    while (*s++); // A la sortie de la boucle le pointeur s pointe sur le caractère nul, 
-                  // car durant la dernière itération dès que le caractère nul est lu il est pris en compte en tant que caractère avant que la boucle ne sarrête
-    return s - start - 1; // raison pour laquelle il faut soutraire le carctère nul du comptage.
+    while (*s++);                               // A la sortie de la boucle le pointeur s pointe sur le caractère nul, 
+                                                // car durant la dernière itération dès que le caractère nul est lu il est pris en compte en tant que caractère avant que la boucle ne sarrête
+    return s - start - 1;                       // retour de la différence entre l'adresse du dernier élément et celui du premier pour connaitre le nombre d'élément contenu dans la chaine - 1 (-1 en raison de la soustraction du carctère nul inclus dans le comptage.)
 }
 
 void main(void)
