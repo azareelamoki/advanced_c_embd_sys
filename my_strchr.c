b955f10adb9632f806a702e900616cc6de04d83d
azareelamoki <azareelamoki944@gmail.com>
2026-08-11 18:31:40 +0100

    Little add (On a second way to recode strchr)

--- my_strchr.c
+++ my_strchr.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-// #include <stddef.h>
 
-char *my_strchr(const char *s, int c)
+char *my_strchr(const char *s, int c)   // Approche A
 {
     char *ptr = (char *)s;              // caster le pointeur s en un type char * et copier l'adresse qu'elle contient dans ptr pour correspondre au type de retour de la fonction
     while (*ptr != '\0' && *ptr != c)   // La boucle s'arrête à condition que le caractère soit retrouvé dans la chaine de caractère (on utilise ptr car il detient déjà l'adresse du premier emplacement de la chaine)
@@ -11,6 +10,18 @@ char *my_strchr(const char *s, int c)
         return ptr;                     // On retourne le pointeur (Autrement dit l'adresse de l'emplacement auquel a été indentifié le caractère)
     
     return NULL;                        // Dans le cas où le caractère n'a pas été retrouvé le programme retourne une adresse nul.
+}
+
+char *my_strchr_b(const char *s, int c)   // Approche B
+{
+    char *ptr = (char *)s;              // caster le pointeur s en un type char * et copier l'adresse qu'elle contient dans ptr pour correspondre au type de retour de la fonction
+    while (*ptr != '\0' && *ptr != c)   // La boucle s'arrête à condition que le caractère soit retrouvé dans la chaine de caractère (on utilise ptr car il detient déjà l'adresse du premier emplacement de la chaine)
+        ptr++;                          // incrémentation du pointeur: passage de l'adresse actuelle à la suivante (on parle d'adresse mémoire)
+   
+    if (*ptr != c)                      // À la sortie de la boucle si la valeur pointée est réellement différente du caractere recherché,
+        return NULL;                    // cela veut dire que le caractère n'a pas été retrouvé, le programme retourne une adresse nul. 
+    
+    return ptr;                         // Dans le cas où le caractère est identique, On retourne le pointeur (Autrement dit l'adresse de l'emplacement à laquelle a été indentifié le caractère)
 }
 
 void main(void)
