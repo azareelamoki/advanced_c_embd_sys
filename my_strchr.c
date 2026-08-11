e6512c38789018a88891f360189ca0c068be41ce
azareelamoki <azareelamoki944@gmail.com>
2026-08-11 18:41:10 +0100

    Added a third way of recoding strchr (Minors changes) whit documentation along

--- my_strchr.c
+++ my_strchr.c
@@ -2,7 +2,31 @@
 
 char *my_strchr(const char *s, int c)   // Approche A
 {
-    char *ptr = (char *)s;              // caster le pointeur s en un type char * et copier l'adresse qu'elle contient dans ptr pour correspondre au type de retour de la fonction
+    const char *ptr = s;              // copie de l'adresse contenue dans s, dans ptr.
+    while (*ptr != '\0' && *ptr != c)   // La boucle s'arrête à condition que le caractère soit retrouvé dans la chaine de caractère (on utilise ptr car il detient déjà l'adresse du premier emplacement de la chaine)
+        ptr++;                          // incrémentation du pointeur: passage de l'adresse actuelle à la suivante (on parle d'adresse mémoire)
+   
+    if (*ptr == c)                      // À la sortie de la boucle si la valeur pointée est réellement identique au caractere recherché,
+        return (char *)ptr;             // On retourne le pointeur casté en char * pour correspondre au type de retour de la fonction (Autrement dit l'adresse de l'emplacement à laquelle a été indentifié le caractère)
+    
+    return NULL;                        // Dans le cas où le caractère n'a pas été retrouvé le programme retourne une adresse nul.
+}
+
+char *my_strchr_b(const char *s, int c)   // Approche B
+{
+    const char *ptr = s;                // copie de l'adresse contenue dans s, dans ptr.
+    while (*ptr != '\0' && *ptr != c)   // La boucle s'arrête à condition que le caractère soit retrouvé dans la chaine de caractère (on utilise ptr car il detient déjà l'adresse du premier emplacement de la chaine)
+        ptr++;                          // incrémentation du pointeur: passage de l'adresse actuelle à la suivante (on parle d'adresse mémoire)
+   
+    if (*ptr != c)                      // À la sortie de la boucle si la valeur pointée est réellement différente du caractere recherché,
+        return NULL;                    // cela veut dire que le caractère n'a pas été retrouvé, le programme retourne une adresse nul. 
+    
+    return ((char *)ptr);               // Dans le cas où le caractère est identique, On retourne le pointeur casté en char * pour correspondre au type de retour de la fonction (Autrement dit l'adresse de l'emplacement à laquelle a été indentifié le caractère)
+}
+
+const char *my_strchr(const char *s, int c)   // Approche A
+{
+    const char *ptr = s;                // copie de l'adresse contenue dans s, dans ptr.
     while (*ptr != '\0' && *ptr != c)   // La boucle s'arrête à condition que le caractère soit retrouvé dans la chaine de caractère (on utilise ptr car il detient déjà l'adresse du premier emplacement de la chaine)
         ptr++;                          // incrémentation du pointeur: passage de l'adresse actuelle à la suivante (on parle d'adresse mémoire)
    
@@ -12,21 +36,9 @@ char *my_strchr(const char *s, int c)   // Approche A
     return NULL;                        // Dans le cas où le caractère n'a pas été retrouvé le programme retourne une adresse nul.
 }
 
-char *my_strchr_b(const char *s, int c)   // Approche B
-{
-    char *ptr = (char *)s;              // caster le pointeur s en un type char * et copier l'adresse qu'elle contient dans ptr pour correspondre au type de retour de la fonction
-    while (*ptr != '\0' && *ptr != c)   // La boucle s'arrête à condition que le caractère soit retrouvé dans la chaine de caractère (on utilise ptr car il detient déjà l'adresse du premier emplacement de la chaine)
-        ptr++;                          // incrémentation du pointeur: passage de l'adresse actuelle à la suivante (on parle d'adresse mémoire)
-   
-    if (*ptr != c)                      // À la sortie de la boucle si la valeur pointée est réellement différente du caractere recherché,
-        return NULL;                    // cela veut dire que le caractère n'a pas été retrouvé, le programme retourne une adresse nul. 
-    
-    return ptr;                         // Dans le cas où le caractère est identique, On retourne le pointeur (Autrement dit l'adresse de l'emplacement à laquelle a été indentifié le caractère)
-}
-
 void main(void)
 {
     char s[] ="Bonjour";
-    char * test = my_strchr(s, 'e');
+    char * test = my_strchr_b(s, 'e');
     (test != NULL) ? printf("%s\n", test) : printf("Character Not found !\n");
 }
