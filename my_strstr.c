f6f6f26554aba537c088e71d84ca28c760e8c2cf
azareelamoki <azareelamoki944@gmail.com>
2026-08-12 14:59:57 +0100

    Coded a second aspect of strtstr function

--- my_strstr.c
+++ my_strstr.c
@@ -9,7 +9,7 @@ size_t my_strlen(const char *s) //Approche A
     return i;                                  // Retour de cet indice car la fonction sers à connaitre le nombre d'élément contenus dans un tableau de caractère
 }
 
-char *my_strstr(const char *haystack, const char *needle)
+char *my_strstr(const char *haystack, const char *needle) //Approche A
 {
     const char *tmp;
     const char *tmp2 = needle;
@@ -36,9 +36,29 @@ char *my_strstr(const char *haystack, const char *needle)
     return NULL;
 }
 
+char *my_strstr_b(const char *haystack, const char *needle) //Approche B (recommandée) car plus logique et plus efficace
+{
+    const char *tmp;
+    const char *tmp2;
+
+    while (*haystack != '\0' && *needle != '\0') {
+        tmp = haystack;
+        tmp2 = needle;
+
+        while (*tmp2 != '\0' && *tmp == *tmp2) {
+            tmp++; tmp2++;
+        }
+
+        if (*tmp2 == '\0') return (char *)haystack;
+        haystack++;
+    }
+    return NULL;
+}
+
+
 void main(void)
 {
-    char s[] ="Bonjour";
-    char * test = my_strstr(s, "jo");
+    char s[] ="Bonjojour";
+    char * test = my_strstr_b(s, "jour");
     (test != NULL) ? printf("%s\n", test) : printf("String Not found !\n");
 }
