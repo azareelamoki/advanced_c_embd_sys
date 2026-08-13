0c70e9d36c6e421d7a334eb7891794605ff7a6b7
azareelamoki <azareelamoki944@gmail.com>
2026-08-13 21:30:41 +0100

    Deleted the first case of coding strstr, added few lines of explanation comments

--- my_strstr.c
+++ my_strstr.c
@@ -9,45 +9,18 @@ size_t my_strlen(const char *s) //Approche A
     return i;                                  // Retour de cet indice car la fonction sers à connaitre le nombre d'élément contenus dans un tableau de caractère
 }
 
-char *my_strstr(const char *haystack, const char *needle) //Approche A
+char *my_strstr_b(const char *haystack, const char *needle) //Approche recommandée (car plus logique et plus efficace)
 {
-    const char *tmp;
-    const char *tmp2 = needle;
+    const char *tmp;                                        // initialisation d'un premier pointeur temporaire pour la copie de l'adresse contenue dans haystack
+    const char *tmp2;                                       // initialisation d'un deuxième pointeur temporaire pour la copie de l'adresse contenue dans needle
 
-    if (my_strlen(needle) < 1) return (char *)haystack;
-    
-    while (*haystack != '\0') {
+    if (my_strlen(needle) < 1) return (char *)haystack;     // Retour du pointeur initial: haystack qui pointe vers le debut de la chaine de caractère (car needle est vide donc aucune recherche ne doit etre effectuée)
 
-        if (*haystack == *tmp2) {
-
-            tmp = haystack;
-            needle = tmp2;
-            
-            while (*needle != '\0' && *haystack == *needle) {
-                haystack++;
-                needle++;
-            }
-            
-            if (*needle == '\0') 
-                return (char *)tmp;
-        }
-        haystack++;
-    }
-    return NULL;
-}
-
-char *my_strstr_b(const char *haystack, const char *needle) //Approche B (recommandée) car plus logique et plus efficace
-{
-    const char *tmp;
-    const char *tmp2;
-
-    if (my_strlen(needle) < 1) return (char *)haystack;
-
-    while (*haystack != '\0' && *needle != '\0') {
+    while (*haystack != '\0') {          // Première boucle fonctionnelle à condition que le caractère (pointé par haystack soit différent du caractère nul)
         tmp = haystack;
         tmp2 = needle;
 
-        while (*tmp2 != '\0' && *tmp == *tmp2) {
+        while (*tmp2 != '\0' && *tmp == *tmp2 && *tmp2 != '\0') {
             tmp++; tmp2++;
         }
 
