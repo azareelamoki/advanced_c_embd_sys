5482f4ea78f1ee7de3f8b68fe5adf5066f943ac8
azareelamoki <azareelamoki944@gmail.com>
2026-08-13 22:05:28 +0100

    Full lines of explanation comments to explain how it works

--- my_strstr.c
+++ my_strstr.c
@@ -16,18 +16,20 @@ char *my_strstr_b(const char *haystack, const char *needle) //Approche recommand
 
     if (my_strlen(needle) < 1) return (char *)haystack;     // Retour du pointeur initial: haystack qui pointe vers le debut de la chaine de caractère (car needle est vide donc aucune recherche ne doit etre effectuée)
 
-    while (*haystack != '\0') {          // Première boucle fonctionnelle à condition que le caractère (pointé par haystack soit différent du caractère nul)
-        tmp = haystack;
-        tmp2 = needle;
+    while (*haystack != '\0') {                             // Première boucle; fonctionnelle à condition que: le caractère (pointé par haystack soit différent du caractère nul)
+        tmp = haystack;                                     // copie de l'adresse du caractère pointé par haystack dans tmp
+        tmp2 = needle;                                      // copie de l'adresse du caractère pointé par needle dans tmp2
 
-        while (*tmp2 != '\0' && *tmp == *tmp2 && *tmp2 != '\0') {
-            tmp++; tmp2++;
+        while (*tmp2 != '\0' && *tmp == *tmp2) {            // Deuxième Boucle ; fonctionnelle à condition que : la valeur (le caractère plus précisément) pointée par tmp2 soit différent du carctère nul et que la valeur pointée par tmp soit équivalente celle pointée par tmp2 
+            tmp++; tmp2++;                                  // passage des adresses actuelles aux addresses suivantes (autrement dit passage du caractère atuel au carctère qui suit)
         }
 
-        if (*tmp2 == '\0') return (char *)haystack;
-        haystack++;
+        if (*tmp2 == '\0') return (char *)haystack;         // dès que la valeur pointée par tmp2 à la sortie de la deuxième boucle 
+                                                            // vérifie cette condition (C'est à dire que cette valeur n'est autre que le terminateur nul qui veut dire que tout les caractères de pointés par tmp2 ont trouvé d'quivalence), 
+                                                            // le programme retourne le pointeur haystack qui représente le pointeur initial et l'adresse de repère.
+        haystack++;                                         // Dans le cas ou la condition n'est pas vérifiée, le pointeur haystack pointe vers le caractère suivant afin de reprendre le processus ci dessus
     }
-    return NULL;
+    return NULL;                                            // À la sortie de la première boucle (ce qui veut dire que tout la chaine initial a été belle et bien traitée sans obtenir une correspondance en son sein) le programme retourne un pointeur null.
 }
 
 
